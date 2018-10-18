09ba0312de6b849b7e7ba06eb34941d23f0cf77b
Robert Todea <robert.todea@e-spres-oh.com>
2018-10-18 14:49:44 +0300

    Add local image and use caman inside angular

--- src/app/caman/caman.component.ts
+++ src/app/caman/caman.component.ts
@@ -1,18 +1,27 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+declare var Caman: any;
 
 @Component({
   selector: 'app-caman',
   templateUrl: './caman.component.html',
   styleUrls: ['./caman.component.css']
 })
-export class CamanComponent implements OnInit {
+export class CamanComponent implements OnInit, AfterViewInit {
   @Input() image: string;
 
   @ViewChild('canvas') canvasElementRef: ElementRef;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    console.log(this.canvasElementRef.nativeElement);
+    Caman(this.canvasElementRef.nativeElement, this.image, function () {
+      this.render();
+    });
+  }
 }
